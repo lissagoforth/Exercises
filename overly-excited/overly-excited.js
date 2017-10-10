940286feb3f368d89cd76f5a9487c75efbf2a36e
Lissa Goforth <lissagoforth@gmail.com>
2017-10-10 13:53:49 -0500

    actually completed overly-excited exercise

--- overly-excited/overly-excited.js
+++ overly-excited/overly-excited.js
@@ -1,21 +1,22 @@
 // Create an array that contains the words in the sentence
 let sentence = ["You", "can't", "sit", "with", "us", "Becky,", "you", "bitch.", "It's", "not", "sweatpant", "day."];
-
+let nextSentence = ["I", "love", "the", "movie", "'Mean", "Girls.'"]
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+let addExcitement = (theWordArray, specialCharacter) => {
     let stringBuilder = ""
+    let punctuation = specialCharacter
     
     for (i = 0; i < theWordArray.length; i++) {
         const punctuationPosition = i + 1
         const  word = theWordArray[i]
 
         if (punctuationPosition % 3 === 0) {
-            const addBang = punctuationPosition / 3
-            stringBuilder += word + "!".repeat(addBang) + " "
+            const addPunctuation = punctuationPosition / 3
+            stringBuilder += word + punctuation.repeat(addPunctuation) + " "
         } else {
             stringBuilder += word + " "
         }
@@ -26,4 +27,5 @@ function addExcitement (theWordArray) {
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence);    
+// let addExcitement = () => {nextSentence, "$"};
+//  console.log(addExcitement(nextSentence, "$"))   
